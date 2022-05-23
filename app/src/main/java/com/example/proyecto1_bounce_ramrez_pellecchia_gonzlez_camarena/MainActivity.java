20e598403f52c39c7aa1704a9ababcd22b2850ec
Raul Ramirez <raul.ramirez@utp.ac.pa>
2022-05-22 20:04:18 -0500

    Se agregó el activity main para el proyecto 1

---
+++ app/src/main/java/com/example/proyecto1_bounce_ramrez_pellecchia_gonzlez_camarena/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.proyecto1_bounce_ramrez_pellecchia_gonzlez_camarena;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/test/java/com/example/proyecto1_bounce_ramrez_pellecchia_gonzlez_camarena/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.example.proyecto1_bounce_ramrez_pellecchia_gonzlez_camarena;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
