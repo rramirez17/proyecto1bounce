525b5f1c4bf2411f8541f2a47601a1f7c246b335
Raul Ramirez <raul.ramirez@utp.ac.pa>
2022-05-24 09:40:51 -0500

    Se agregó la lógica inicial de la animación Bounce

--- app/src/main/java/com/example/proyecto1_bounce_ramrez_pellecchia_gonzlez_camarena/MainActivity.java
+++ app/src/main/java/com/example/proyecto1_bounce_ramrez_pellecchia_gonzlez_camarena/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.proyecto1_bounce_ramrez_pellecchia_gonzlez_camarena;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private Button button1;
+    private ImageView imageView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView imageView = (ImageView) findViewById(R.id.persona);
+        button1 = (Button) findViewById(R.id.button);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),R.animati);
+                imageView.startAnimation(animation);
+            }
+        });
     }
 }
