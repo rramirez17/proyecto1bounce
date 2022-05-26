7918021991ce1a42932f9f127ddf2a3e7833e2b2
Angela Pellecchia <a.pellecchia18@gmail.com>
2022-05-25 21:11:38 -0500

    Se agregaron las funcionalidades para el botón Angela y las imágenes

--- app/src/main/java/com/example/proyecto1_bounce_ramrez_pellecchia_gonzlez_camarena/MainActivity.java
+++ app/src/main/java/com/example/proyecto1_bounce_ramrez_pellecchia_gonzlez_camarena/MainActivity.java
@@ -14,7 +14,7 @@ import android.widget.ImageView;
 public class MainActivity extends AppCompatActivity {
 
 
-    private Button button1;
+    private Button button1, button2;
     private ImageView imageView;
 
 
@@ -54,8 +54,39 @@ public class MainActivity extends AppCompatActivity {
                         }, 850);
                     }
                 }, 700);
+            }
+        });
 
+        button2 = (Button) findViewById(R.id.button2);
 
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imageView.setImageResource(R.drawable.bounce_angela_1);
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    public void run() {
+                        imageView.setImageResource(R.drawable.bounce_angela_2);
+                        Animation bounce_up = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.bounce_up);
+                        imageView.startAnimation(bounce_up);
+                        Handler handler = new Handler();
+                        handler.postDelayed(new Runnable() {
+                            public void run() {
+                                imageView.setImageResource(R.drawable.bounce_angela_3);
+                                Animation bounce_down = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.bounce_down);
+                                imageView.startAnimation(bounce_down);
+                                Handler handler = new Handler();
+                                handler.postDelayed(new Runnable() {
+                                    public void run() {
+                                        Animation get_off_trampoline = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.get_off_trampoline);
+                                        imageView.startAnimation(get_off_trampoline);
+                                        imageView.setImageResource(R.drawable.bounce_angela_4);
+                                    }
+                                }, 1700);
+                            }
+                        }, 850);
+                    }
+                }, 700);
             }
         });
     }
