cfc1f738e03416b8980724fc04d11b421273ee77
D-Camarena <dcamarena.94@gmail.com>
2022-05-26 20:05:24 -0500

    Se agregó las imágenes y la funcionalidad para el botón David

--- app/src/main/java/com/example/proyecto1_bounce_ramrez_pellecchia_gonzlez_camarena/MainActivity.java
+++ app/src/main/java/com/example/proyecto1_bounce_ramrez_pellecchia_gonzlez_camarena/MainActivity.java
@@ -14,7 +14,7 @@ import android.widget.ImageView;
 public class MainActivity extends AppCompatActivity {
 
 
-    private Button button1, button2;
+    private Button button1, button2, button3;
     private ImageView imageView;
 
 
@@ -25,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
 
         ImageView imageView = (ImageView) findViewById(R.id.persona);
         button1 = (Button) findViewById(R.id.button);
+        button3 = (Button) findViewById(R.id.button3);
 
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -81,6 +82,37 @@ public class MainActivity extends AppCompatActivity {
                                         Animation get_off_trampoline = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.get_off_trampoline);
                                         imageView.startAnimation(get_off_trampoline);
                                         imageView.setImageResource(R.drawable.bounce_angela_4);
+                                    }
+                                }, 1700);
+                            }
+                        }, 850);
+                    }
+                }, 700);
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imageView.setImageResource(R.drawable.bounce_david_1);
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    public void run() {
+                        imageView.setImageResource(R.drawable.bounce_david_2);
+                        Animation bounce_up = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.bounce_up);
+                        imageView.startAnimation(bounce_up);
+                        Handler handler = new Handler();
+                        handler.postDelayed(new Runnable() {
+                            public void run() {
+                                imageView.setImageResource(R.drawable.bounce_david_3);
+                                Animation bounce_down = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.bounce_down);
+                                imageView.startAnimation(bounce_down);
+                                Handler handler = new Handler();
+                                handler.postDelayed(new Runnable() {
+                                    public void run() {
+                                        Animation get_off_trampoline = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.get_off_trampoline);
+                                        imageView.startAnimation(get_off_trampoline);
+                                        imageView.setImageResource(R.drawable.bounce_david_4);
                                     }
                                 }, 1700);
                             }
